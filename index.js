5337f0eb813bcfaadffeec89e1d0b6a64d7bde3d
bzkarim250 <bzkarim250@gmail.com>
2024-02-20 14:19:14 +0200

    feat(ft-mybrand-mobile):My brand responsiveness
    - implement my brand landingpage responsiveness
    
    [WIP]

---
+++ index.js
@@ -0,0 +1,23 @@
+let nav = document.querySelector(".header__nav");
+let burger = document.querySelector(".hamburger");
+let burgerFirstLine = document.querySelector(".hamburger__line:nth-child(1)");
+let burgerSecondLine = document.querySelector(".hamburger__line:nth-child(2)");
+let burgerThirdLine = document.querySelector(".hamburger__line:nth-child(3)");
+
+burger.addEventListener("click", function () {
+  burgerFirstLine.classList.toggle("hamburger__line-1");
+  burgerSecondLine.classList.toggle("hamburger__line-2");
+  burgerThirdLine.classList.toggle("hamburger__line-3");
+
+  nav.classList.toggle("display__nav");
+});
+
+window.addEventListener("scroll", function () {
+  if (nav.classList.contains("display__nav")) {
+    burgerFirstLine.classList.remove("hamburger__line-1");
+    burgerSecondLine.classList.remove("hamburger__line-2");
+    burgerThirdLine.classList.remove("hamburger__line-3");
+    nav.classList.remove("display__nav");
+    navBall.classList.remove("display__nav");
+  }
+});
